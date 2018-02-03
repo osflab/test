b7c0d9a5390893e1261b1f6a76254d079b48b9bc
Guillaume Ponçon <gponcon@gmail.com>
2018-02-03 16:38:14 +0100

    Autoloading strategy updated

--- run-tests.php
+++ run-tests.php
@@ -5,12 +5,11 @@
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', 'development');
 set_include_path(realpath(__DIR__ . '/../..') . PATH_SEPARATOR . get_include_path());
 
-// Autoloaders
-if (file_exists(__DIR__ . '/../Loader.php')) {
-    require_once __DIR__ . '/../Loader.php';
-}
+// Autoloader
 if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
+} else if (file_exists(__DIR__ . '/../Loader/autoload.php')) {
+    require_once __DIR__ . '/../Loader/autoload.php';
 }
 
 // Translate function simulation
@@ -19,4 +18,4 @@ function __($txt) { return $txt; }
 // Run
 $dir = isset($_SERVER['argv'][1]) ? realpath($_SERVER['argv'][1]) : getcwd();
 $filter = isset($_SERVER['argv'][2]) ? '/' . $_SERVER['argv'][2] . '/' : null;
-Osf\Test\Runner::runDirectory($dir, '/Test', $filter) || exit(1);
+\Osf\Test\Runner::runDirectory($dir, '/Test', $filter) || exit(1);
